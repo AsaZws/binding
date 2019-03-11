8d70fceea1aa77f910d8c3e4b70762c480ba1b94
AsaZws <zhouws.com@gmail.com>
2019-03-11 10:18:58 +0800

    精简代码，PC端键盘也能很好兼容

--- input1.4/js/binding.js
+++ input1.4/js/binding.js
@@ -1,120 +1,76 @@
-
+/* 
+车牌数字键盘
+name：Asa_Zhou
+data：2019/3/8修改
+*/
 ;$(function ($) {
-    // 获取键盘上的关闭按钮
-    var oShut = $("#shut");
-    // 获取键盘的id
-    var oShutkey = $("#shutkey");
-    // 获取车牌输入框的id
-    var oPlate = $("#plate");
-    // 获取数字拼音软键盘的每个值
-    var oOli = $("#keyboard li");
-    // 获取地名简称软键盘的每个值
-    var oOlia = $("#keyboarda li");
-    // 获取地方简称键盘的id
-    var oArea = $("#area");
-    // 获取地方简称的关闭按钮
-    var oShuta = $("#shuta");
-    // 获取车牌号id
-    var oBinding = $("#binding");
+    var oShut = $("#shut");         // 数字字母的关闭按钮
+    var oShutkey = $("#shutkey");   // 数字字母键盘
+    var oPlate = $("#plate");       // 车牌输入框
+    var oOli = $("#keyboard li");   // 数字字母键盘的每个li
+    var oArea = $("#area");         // 地名简称键盘
+    var oOlia = $("#keyboarda li"); // 地名简称键盘上的每个li
+    var oShuta = $("#shuta");       // 地方简称的关闭按钮
 
-    // 点击关闭，关闭键盘
-    oShut.click(function () {
+    oShut.click(function () {   // 点击关闭，关闭数字字母键盘
         oShutkey.slideUp(100);
     });
-    // 点击关闭，关闭地名键盘
-    oShuta.click(function () {
+    oShuta.click(function () {  // 点击关闭，关闭地名简称键盘
         oArea.slideUp(100);
     });
 
-    // 切换车牌号码框的函数
-    (function () {
-
-        // 隐藏数字拼音键盘
-        function oshutkey() {
+    (function () {                  // 切换车牌号码框的函数
+        function oshutkey() {       // 隐藏数字拼音键盘
             oShutkey.slideUp(200);
         };
-        // 显示地名简称键盘
-        function oarea() {
+        function oarea() {          // 显示地名简称键盘
             oArea.slideDown(200);
         };
 
-        // 定义车牌数组
-        var oarr = [];
-        // 定义车牌字符串
-        var ostr;
-
-        // 遍历li
-        for (var i = 0; i < oOli.length; i++) {
-            // 获取键盘li的索引
-            oOli[i].index = i;
-            // 给每个li添加点击事件
-            oOli[i].onclick = function () {
-                // 每点击一次就把当前的值添加到后面
-                oarr.push(this.innerText);
-                // 把数组中间的逗号变空,添加到字符串
-                ostr = oarr.join("");
-                // 把新数组添加到输入框中去
-                oPlate.attr("value", ostr);
-                // 当长度大于7的时候，自动删除最后一位
-                if ( oarr.length > 7 ) {
+        var oarr = ["粤","B"];  // 定义车牌数组，默认为深圳牌
+        var ostr;               // 定义车牌字符串
+        for (var i = 0; i < oOli.length; i++) {  // 遍历数字字母车牌
+            oOli[i].index = i;                   // 获取键盘li的索引
+            oOli[i].onclick = function () {      // 给每个键添加点击事件
+                if ( oarr.length > 7 ) {         // 当长度大于8的时候，自动删除最后一位
                     oarr.length = 7;
                 };
-                console.log(ostr);
+                oarr.push(this.innerText);       // 每点击一次就把当前的值添加到后面
+                ostr = oarr.join("");            // 把数组中间的逗号变空,添加到字符串
+                oPlate.attr("value", ostr);      // 把新数组添加到输入框中去
+                console.log(oarr);
             }
         }
-        // 删除
-        $('#delet').click(function () {
-            // 点击删除最后一个
-            oarr.splice(-1,1);
-            // 删除字符串中间的逗号
-            ostr = oarr.join("");
-            // 把字符串添加到输入框里面
-            oPlate.val(ostr);
-            // 如果回删到
-            if (ostr.length<1) {
-                // 调用隐藏拼音函数
+        $('#delet').click(function () {  // 删除
+            oarr.splice(-1,1);           // 点击删除最后一个
+            console.log(oarr);
+            ostr = oarr.join("");        // 删除字符串中间的逗号
+            oPlate.val(ostr);            // 把字符串添加到输入框里面
+            if (ostr.length<1) {         // 如果回删到只剩一位，隐藏数字字母键盘，显示地方简称键盘
                 oshutkey();
-                // 调用显示地名函数
                 oarea();
             }
             console.log(ostr);
         })
-
-        // 遍历地名
-        for (i = 0; i < oOlia.length; i++) {
-            // 给地名键盘绑定点击事件
-            oOlia[i].onclick = function () {
-                // 每次点击都把当前点击值添加到后面
-                oarr.push(this.innerText);
-                // 把字符串中间的逗号变空
-                ostr = oarr.join("")
-                // 把新加在数组中的数添加到输入框中去
-                oPlate.attr("value", ostr);
-                // 当输入框中长度大于1，关闭地名简称键盘，自动调出数字拼音键盘
-                if ( ostr.length > 0 ) {
+        for (i = 0; i < oOlia.length; i++) {      // 遍历地名
+            oOlia[i].onclick = function () {      // 给地名键盘绑定点击事件
+                oarr.push(this.innerText);        // 每次点击都把当前点击值添加到后面
+                ostr = oarr.join("");             // 把字符串中间的逗号变空
+                oPlate.attr("value", ostr);       // 把新加在数组中的数添加到输入框中去
+                if ( oPlate.val().length > 0 ) {  // 当输入框中长度大于1，关闭地名简称键盘，自动调出数字拼音键盘
                     oArea.slideUp(200);
                     oShutkey.slideDown(200);
                 }
             }
         }
-        
-        // 给车牌框绑定点击事件
-        oPlate.bind("click", function () {
-            if ( oarr.length > 0 ) {
+        oPlate.bind("click", function () {       // 给车牌框绑定点击事件
+            if ( oPlate.val().length > 0 ) {     // 如果车牌框里面的车牌号码长度大于1，则显示数字字母键盘，隐藏地名简称键盘
                 oShutkey.slideDown(200);
                 oArea.slideUp(200);
             } else {
-                // 调用显示数字拼音函数
-                oshutkey();
-                // 调用隐藏地名简称函数
-                oarea();
+                oArea.slideDown(200);
+                oShutkey.slideUp(200);
             };
-        });
-
-        // 点击获取输入的车牌号
-        oBinding.click(function () {
-            // 获取到车牌号码
-            // oPlateNum.text(ostr);
         });
     })();
 });
--- js/binding.js
+++ js/binding.js
@@ -1,194 +1,96 @@
+/**
+ * 只在车牌页面加载
+ * 车牌数字键盘js
+ * name:Asa_Zhou
+ * data:2019/3/8
+ * 
+ */
+(function($) {
+  var oShut = $("#shut");         // 数字字母键盘关闭按钮
+  var oShutkey = $("#shutkey");   // 数字字母键盘
+  var oPli = $("#plate li");      // 车牌号码输入框
+  var oOli = $("#keyboard li");   // 数字字母键盘的li
+  var oArea = $("#area");         // 地名简称键盘
+  var oOlia = $("#keyboarda li"); // 地名简称键盘的li
+  var oShuta = $("#shuta");       // 地名简称的关闭按钮
 
-window.onload = function () {
-
-    // 初始化rem单位
-    (function () {
-        document.addEventListener('DOMContentLoaded', function () {
-            function htmlsize () {
-                let html = document.documentElement;
-                let windowWidth = html.clientWidth;
-                if (windowWidth <= 750) {
-                    html.style.fontsize = windowWidth / 7.5 + 'px';
-                    // 等价于html.style.fontSize = windowWidth / 750 * 100 + 'px';
-                } else {
-                    html.style.fontSize = 100 + 'px';
-                }
-            }
-            htmlsize();
-            window.onresize = function () {
-                htmlsize()
-            }
-        }, false)
-    })();
-
-    
-    (function ($) {
-        // 获取键盘上的关闭按钮
-        var oShut = $("#shut");
-        // 获取键盘的id
-        var oShutkey = $("#shutkey");
-        // 获取车牌框切换的id
-        var oPlate = $("#plate");
-        // 获取号码牌的li
-        var oPli = $("#plate li");
-        // 获取数字拼音软键盘的每个值
-        var oOli = $("#keyboard li");
-        // 获取地名简称软键盘的每个值
-        var oOlia = $("#keyboarda li");
-        // 获取地方简称键盘的id
-        var oArea = $("#area");
-        // 获取地方简称的关闭按钮
-        var oShuta = $("#shuta");
-        // 获取查询缴费的id
-        var oBinding = $("#binding");
-        // 获取显示车牌号的id
-        var oPlateNum = $("#platenum");
-        // 获取弹出框的id
-        var oDetail3 = $("#detail3");
-        // 获取小红叉的id
-        var oCancelParking3 = $("#CancelParking3");
-        // 获取背景灰色的id
-        var oPoplayer3 = $("#poplayer3");
-
-        // 点击关闭，关闭键盘
-        oShut.click(function () {
-            oShutkey.slideUp(100);
-        });
-        // 点击关闭，关闭地名键盘
-        oShuta.click(function () {
-            oArea.slideUp(100);
-        });
-
-        // 切换车牌号码框的函数
-        (function () {
-            // 定义车牌框变量
-            var oPok = 2;
-            // 定义键盘变量
-            var oOlok;
-
-            // 给车牌框绑定点击事件
-            oPli.bind("click", function () {
-                // 获取车牌框里面的索引值
-                var index = $(this).index();
-                // 点击哪个框就给哪个框添加绿色框，并去掉其他绿色框
-                $(this).addClass("active").siblings().removeClass("active");
-                oPok = index;
-                // 隐藏新能源信息
-                function onew() {
-                    $(".new").show(50);
-                };
-                // 隐藏地名简称键盘
-                function oarea() {
-                    oArea.slideUp(200);
-                };
-                // 显示数字拼音键盘
-                function oshutkey() {
-                    oShutkey.slideDown(200);
-                };
-
-                // 当点击第一个的时候，调出地名简称键盘
-                if ( oPok === 0 ) {
-                    onew();
-                    oArea.slideDown(200);
-                    oShutkey.slideUp(200);
-                // 当点击最后一个的时候隐藏自己的innerText值
-                } else if ( oPok === 7 ) {
-                    $(".new").hide(100);
-                    oarea()
-                    oshutkey()
-                // 当点击第二个的时候，弹出数字拼音键盘
-                } else if ( oPok === 1 ) {
-                    onew();
-                    oarea()
-                    oshutkey()
-                // 当点击第7个的时候全部显示
-                } else if(oPok === 6) {
-                    onew();
-                    oarea()
-                    oshutkey()
-                // 当点击其他车牌框的时候调出数字拼音键盘
-                } else {
-                    onew();
-                    oarea()
-                    oshutkey()
-                };
-            });
-            
-            // 给数字拼音键盘绑定点击事件
-            oOli.bind("click", function () {
-                // 获取键盘的索引
-                var index = $(this).index();
-                oOlok = index;
-                // 绿色框加到下一个
-                function opli() {
-                    oPli.eq(oPok).addClass("active").siblings().removeClass("active");
-                };
-                // 点击删除按钮，往后回删内容
-                if ( oOlok === 38 ) {
-                    oPli.eq(oPok).html("");
-                    oPok --;
-                    opli();
-                    // 当回删到第一个的时候隐藏数字拼音键盘，显示地名简称键盘
-                    if ( oPok === 0 ) {
-                        oArea.slideDown(200);
-                        oShutkey.slideUp(200);
-                        // 
-                    } else if ( oPok < 8 ) {
-                        oPli.eq(7).html('<div class="new"><span>+</span><i>新能源</i></div>');
-                    };
-                }else {
-                    // 点击数字拼音键盘替换获取车牌框索引值的值
-                    oPli.eq(oPok).html(this.innerText);
-                    // 每点击一次增加一次索引
-                    oPok ++;
-                    // 每点击一次绿色框跳转到下一个
-                    opli();
-                }
-                // 当绿色框达到第7个的时候隐藏绿色框，并隐藏键盘
-                if (oPok > 6) {
-                    oShutkey.slideUp(200);
-                    oPli.removeClass("active");
-                };
-            });
-
-            // 给地名键盘绑定点击事件
-            oOlia.bind("click", function () {
-                // 点击简称键盘获取车牌索引值的值
-                oPli.eq(oPok).html(this.innerText);
-                // 点击一次增加一次索引
-                oPok ++;
-                if ( oPok === 1 ) {
-                    oArea.slideUp(200);
-                    oShutkey.slideDown(200);
-                };
-                // 点击一次绿色框跳转到下一个
-                oPli.eq(oPok).addClass("active").siblings().removeClass("active");
-            });
-
-            // 点击查询缴费弹出框，并获取输入的车牌号
-            oBinding.click(function () {
-                var oStr;
-                // 判断车牌最后一位长度是否大于1
-                if ( oPli.eq(7).text().length > 1 ) {
-                    // 如果大于1，显示前7位
-                    oStr = $("#plate li:lt(7)").text();
-                    oPlateNum.text(oStr);
-                } else {
-                    // 最后一位输入车牌就显示8位
-                    oStr = $("#plate li:lt(8)").text();
-                    oPlateNum.text(oStr);
-                };
-                // 显示弹框
-                oDetail3.show(100);
-                // 显示背景层
-                oPoplayer3.show();
-            });
-        })();
-
-        // 点击小红叉隐藏页面
-        oCancelParking3.click(function () {
-            oDetail3.hide(100);
-            oPoplayer3.hide();
-        });
-    })(jQuery); 
-}
+  oShut.click(function() {    // 点击关闭，关闭数字字母键盘
+    oShutkey.slideUp(100);
+  });
+  oShuta.click(function() {  // 点击关闭，关闭地名简称键盘
+    oArea.slideUp(100);
+  });
+  (function() {                       // 切换车牌号码框的函数
+    var oPok = 2;                     // 定义车牌框从哪个位置开始的变量
+    oPli.bind("click", function() {   // 给车牌框绑定点击事件
+      var index = $(this).index();    // 获取车牌框里面的索引值
+      $(this).addClass("active").siblings().removeClass("active");  // 点击哪个框就给哪个框添加颜色框，并去掉其他颜色框
+      oPok = index;
+      function onew() {           // 隐藏新能源信息
+        $(".new").show(50);
+      }
+      function oarea() {          // 隐藏地名简称键盘
+        oArea.slideUp(200);
+      }
+      function oshutkey() {       // 显示数字字母键盘
+        oShutkey.slideDown(200);
+      }
+      if (oPok === 0) {           // 当点击第一个的时候，调出地名简称键盘
+        onew();
+        oArea.slideDown(200);
+        oShutkey.slideUp(200);
+      } else if (oPok === 7) {    // 当点击最后一个的时候隐藏新能源车牌的innerText值
+        $(".new").hide(100);
+        oarea();
+        oshutkey();
+      } else if (oPok === 1) {    // 当点击第二个的时候，弹出数字字母键盘
+        onew();
+        oarea();
+        oshutkey();
+      } else if (oPok === 6) {    // 当点击第7个的时候全部显示
+        onew();
+        oarea();
+        oshutkey();
+      } else {                    // 当点击其他车牌框的时候调出数字字母键盘
+        onew();
+        oarea();
+        oshutkey();
+      }
+    });
+    oOli.bind("click", function() {    // 给数字字母键盘绑定点击事件
+      var index = $(this).index();     // 获取键盘的索引
+      oOlok = index;
+      function opli() {                // 颜色框加到下一个
+        oPli.eq(oPok).addClass("active").siblings().removeClass("active");
+      }
+      if ($(this).html() === "删除") {  // 点击删除按钮，往后回删内容
+        oPli.eq(oPok).html("");
+        oPok--;
+        opli();
+        if (oPok === 0) {              // 当回删到第一个的时候隐藏数字字母键盘，显示地名简称键盘
+          oArea.slideDown(200);
+          oShutkey.slideUp(200);
+        } else if (oPok < 8) {
+          oPli.eq(7).html('<div class="new"><span>+</span><i>新能源</i></div>');
+        }
+      } else {                        // 点击数字字母键盘替换获取车牌框索引值的值
+        oPli.eq(oPok).html(this.innerText);
+        oPok++;            // 每点击一次增加一次索引
+        opli();            // 每点击一次颜色框跳转到下一个
+      }
+      if (oPok > 6) {     // 当颜色框达到第7个的时候隐藏颜色框，并隐藏键盘
+        oShutkey.slideUp(200);
+        oPli.removeClass("active");
+      }
+    });
+    oOlia.bind("click", function() {        // 给地名简称键盘绑定点击事件
+      oPli.eq(oPok).html(this.innerText);   // 点击地名简称键盘获取车牌索引值的值
+      oPok++;                               // 点击一次增加一次索引
+      if (oPok === 1) {
+        oArea.slideUp(200);
+        oShutkey.slideDown(200);
+      }
+      oPli.eq(oPok).addClass("active").siblings().removeClass("active");   // 点击一次颜色框跳转到下一个
+    });
+  })();
+})(jQuery);
