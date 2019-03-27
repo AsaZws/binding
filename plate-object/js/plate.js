f123b722bc0fa0793f0db3ad984ff4c0314aa404
zhouweisheng <zhouws.com@gmail.com>
2019-03-27 08:30:45 +0800

    json

--- plate-object/js/plate.js
+++ plate-object/js/plate.js
@@ -5,9 +5,23 @@ data：2019/3/25修改
 */
 'use strict';
 // 车牌键盘数组
-var Place_name = ["京","津","沪","渝","蒙","新","藏","宁","桂","黑","吉","辽","晋","冀","青","鲁","豫","苏","皖","浙","闽","赣","湘","鄂","粤","琼","甘","陕","贵","云","川"];
-var Letter = ["1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","o","P","港","澳","A","S","D","F","G","H","J","K","L","学","Z","X","C","V","B","N","M","警"];
-var _Okeyboard = '<div class="keyboard"> \
+var Keyboard = {
+        Place_name : ["京","津","沪","渝","蒙","新","藏","宁","桂","黑","吉","辽","晋","冀","青","鲁","豫","苏","皖","浙","闽","赣","湘","鄂","粤","琼","甘","陕","贵","云","川"],
+        Letter: ["1","2","3","4","5","6","7","8","9","0","Q","W","E","R","T","Y","U","I","o","P","港","澳","A","S","D","F","G","H","J","K","L","学","Z","X","C","V","B","N","M","警"],
+
+}
+var oLi = "";
+for (var i in Keyboard.Place_name) {
+    oLi += "<li>" + Keyboard.Place_name[i] + "</li>";
+    if (i%10 == 0) {
+        oLi += "</ul><ul>"
+    }
+}
+oLi = "<ul>" + oLi + "</ul>"
+console.log(oLi)
+// 获取元素
+function Plate(id) {
+    var _Okeyboard = '<div class="keyboard"> \
                     <div class="shut-down">关闭</div> \
                     <div class="place-name"> \
                         <ul></ul> \
@@ -16,8 +30,6 @@ var _Okeyboard = '<div class="keyboard"> \
                         <ul></ul> \
                     </div> \
                  </div>';
-// 获取元素
-function Plate(id) {
     var Oplate = document.getElementById(id);                           // 获取对象
     this.Ovalue = Oplate.getElementsByTagName("input")[0];                 // 获取输入框
     this.Okeyboard = Oplate.getElementsByClassName("keyboard");         // 获取键盘
