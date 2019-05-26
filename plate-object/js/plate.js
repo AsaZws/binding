26a814028aceeadbffa6dfefe91ff74ebc75ed18
zhouweisheng <zhouws.com@gmail.com>
2019-05-26 18:37:11 +0800

    渲染键盘

--- plate-object/js/plate.js
+++ plate-object/js/plate.js
@@ -27,28 +27,32 @@ function Cyclic_Li(oLi, arr) {
     }
     str = "<ul>" + oLi + "</ul>";
 }
-Cyclic_Li(str, Letter);
 // 获取元素
 function Plate(id) {
+    var Oplate = document.getElementById(id);                              // 获取对象
+    this.Ovalue = Oplate.getElementsByTagName("input")[0];                 // 获取输入框
+    if(this.Ovalue.value.length < 1) {
+        Cyclic_Li(str, Place_name);
+    } else {
+        Cyclic_Li(str, Letter);
+    }
     var _Okeyboard = '<div class="keyboard"> \
                     <div class="shut-down">关闭</div> \
                     <div class="place-name">'
                     + str +
                     '</div> \
-                    <div class="letter"> \
-                        <ul></ul> \
-                    </div> \
                  </div>';
-    var Oplate = document.getElementById(id);                              // 获取对象
-    this.Ovalue = Oplate.getElementsByTagName("input")[0];                 // 获取输入框
-    this.Okeyboard = Oplate.getElementsByClassName("keyboard");            // 获取键盘
     // this.Oplace_name = Okeyboard.getElementsByClassName("place-name");  // 获取地名键盘
     // this.Oletter = Okeyboard.getElementsByClassName("letter");          // 获取数字字母键盘
     // this.Oshut_down = Okeyboard.getElementsByClassName("shut-down");    // 获取关闭
 
     // Oplate.insertAdjacentHTML("afterBegin", _Okeyboard);
     Oplate.innerHTML = Oplate.innerHTML + _Okeyboard;
-    console.log(Oplate);
+    this.Okeyboard = Oplate.getElementsByClassName("keyboard")[0];          // 渲染完成获取键盘
+    console.log(this.Okeyboard)
+    this.PlaceNameLastLi = Oplate.getElementsByClassName("place-name")[0].lastChild.lastChild;
+    this.PlaceNameLastLi.className = "lastli";
+    console.log(this.PlaceNameLastLi);
 }
 // 点击
 Plate.prototype.OkeyboardClick = function(Okey) {
