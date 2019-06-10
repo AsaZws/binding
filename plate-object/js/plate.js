33cbfe87e9aa67e94e056f4dfcbe2dbfdfac3174
AsaZws <zhouws.com@gmail.com>
2019-06-10 22:57:30 +0800

    组合

--- plate-object/js/plate.js
+++ plate-object/js/plate.js
@@ -8,25 +8,24 @@ data：2019/3/25修改
 var place_name = [['京','津','沪','渝','蒙','新','藏','宁','桂','黑'],['吉','辽','晋','冀','青','鲁','豫','苏','皖','浙'],['闽','赣','湘','鄂','粤','琼','甘','陕','贵','云'],['川']];
 var letter = [['1','2','3','4','5','6','7','8','9','0'],['Q','W','E','R','T','Y','U','P','港','澳'],['A','S','D','F','G','H','J','K','L','学'],['Z','X','C','V','B','N','M','警','回删']];
 
-// 存循环键盘数组的值
-var str = '';
-function cyclicLi(oLi, arr) {
-    for (var i in arr) {
-        for (var j in arr[i]) {
-            oLi += '<li>' + arr[i][j] + '</li>';
-            if(j % 10 == 9) {
-                oLi += '</ul><ul>';
-            }
-        }
-    }
-    str = '<ul>' + oLi + '</ul>';
-}
 // 获取元素dom
 function Plate(id) {
     var _this = this;                                                // 保存this
     this._plate = document.getElementById(id);                       // 获取对象
     this._value = this._plate.getElementsByTagName('input')[0];      // 获取输入框
-    var ostr = this._value.value;  // 输入框的车牌
+    var ostr = this._value.value;   // 输入框的车牌
+    var str = '';
+    function cyclicLi(oLi, arr) {   // 存循环键盘数组的值
+        for (var i in arr) {
+            for (var j in arr[i]) {
+                oLi += '<li>' + arr[i][j] + '</li>';
+                if(j % 10 == 9) {
+                    oLi += '</ul><ul>';
+                }
+            }
+        }
+        str = '<ul>' + oLi + '</ul>';
+    }
     function show(){
         ostr.length < 1 ? cyclicLi(str, place_name) : cyclicLi(str, letter);
         console.log(1);
@@ -41,7 +40,7 @@ function Plate(id) {
     this.okeyboard = this._plate.getElementsByClassName('keyboard')[0];                                 // 渲染完成获取键盘
     this.placeNameLi = this._plate.getElementsByClassName('place-name')[0].getElementsByTagName('li');  // 获取所有li
     this.shutDown = this._plate.getElementsByClassName('shut-down')[0];                                 // 获取关闭
-    this.Oplace_name = this._plate.getElementsByClassName('place-name')[0];                             // 获取键盘
+    this.oplace_name = this._plate.getElementsByClassName('place-name')[0];                             // 获取键盘
 
     this.shutDown.onclick = function () {       // 给关闭按钮添加点击事件
         _this.hiddenKeyboard();
@@ -50,30 +49,33 @@ function Plate(id) {
         _this.blockKeyboard();
     }
     var oarr = ostr.split('');
+    // 循环数字键盘
     for(var i=0; i<this.placeNameLi.length; i++) {
         this.placeNameLi[i].index = i;
+        // 获取循环中最后的一个索引
         var plength = this.placeNameLi.length-1;
         this.placeNameLi[plength].className = 'lastli';
-
-        this.placeNameLi[i].onclick = function () {
-            if(oarr.length > 7) {oarr.length = 7}
-            oarr.push(this.innerHTML);
-            ostr = oarr.join('');
+        // 给循环的每个li添加点击事件，并将值push到输入框中
+        this.placeNameLi[i].onclick = function (e) {
+            if(oarr.length > 7) {oarr.length = 7};
+            console.log(e);
+            oarr.push(this.innerHTML)
+            ostr = oarr.join('')
             _this._value.value = ostr;
-            
+            // console.log(_this.placeNameLi[plength])
         }
-        this.placeNameLi[plength].onclick = function(){
-            oarr.splice(-1,1);
-            ostr = oarr.join('');
-            _this._value.value = ostr;
-            if(ostr.length<1){
-                console.log("该弹车牌键盘啦！")
-                str = '';
-                // cyclicLi(str, place_name);
-                show()
-                _this.Oplace_name.innerHTML = str;
-            }
-        }
+        // 给最后一个删除按钮添加点击事件，并将push的值从输入框中删除
+        // this.placeNameLi[plength].onclick = function(){
+        //     oarr.splice(-1,1);
+        //     ostr = oarr.join('');
+        //     _this._value.value = ostr;
+        //     if(ostr.length<1){
+        //         console.log("该弹车牌键盘啦！")
+        //         str = '';
+        //         show()
+        //         _this.oplace_name.innerHTML = str;
+        //     }
+        // }
     }
 }
 
