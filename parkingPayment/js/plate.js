426599b64ee4d53f6fb7136cb753f9614c2a0e23
AsaZws <zhouws.com@gmail.com>
2019-09-25 21:37:56 +0800

    原型

--- parkingPayment/js/plate.js
+++ parkingPayment/js/plate.js
@@ -1,195 +1,201 @@
 var PVS = ['京', '津', '沪', '渝', '蒙', '新', '藏', '宁', '桂', '黑', '吉', '辽', '晋', '冀', '青', '鲁', '豫', '苏', '皖', '浙', '闽', '赣', '湘', '鄂', '琼', '甘', '陕', '粤', '云', '贵', '川', '', '', '', '', '', '', '', '<span>x</span>'];
 var NUM = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'P', '港', '澳', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '学', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '警', '<span>x</span>'];
-var plate_umber = ['', '', '', '', '', '', '', ''];
+var PNUM = ['', '', '', '', '', '', '', ''];
 function Plate(id) {
-	this._plate = document.getElementById(id);                   								// 获取对象
-	var _this = this;                                            								// 保存this
-	this.plateNumber = _this._plate.getAttribute("plateNumbers");								// 获取自定义车牌号码
-	// 将自定义属性的车牌值赋给车牌数组
-	for(var i=0; i<_this.plateNumber.length; i++) {
-		plate_umber[i] = _this.plateNumber[i];
-	}
-	// 车牌键盘框索引
-	var plateIndex = _this.plateNumber.length;
-	if(plateIndex < 1) {
-		this._plate.insertAdjacentHTML('beforeend', cyclicLi(PVS));              		// 将循环的键循环进去
-	} else {
-		this._plate.insertAdjacentHTML('beforeend', cyclicLi(NUM));              		// 将循环的键循环进去
-	}
-	// init(plateIndex)
-	this.plateFrame = _this._plate.getElementsByClassName("plate-frame")[0];  	// 车牌键盘框
-	this.plateFrame.innerHTML = placeStrs(plate_umber);												  // 创建车牌键盘框
-	this.plateFrameLi = _this.plateFrame.getElementsByTagName("li");						// 键盘框li
-	this.okeyboard = _this._plate.getElementsByClassName('keyboard')[0];        // 渲染完成获取键盘
-	this.placeLetter = _this._plate.getElementsByClassName('place-letter')[0];  // 获取地名键盘
-	this.placeLetterLi = _this.placeLetter.getElementsByTagName('li');          // 获取所有地名li
-	this.shutDown = _this._plate.getElementsByClassName('shut-down')[0];        // 获取关闭
-	// 循环数组
-	function placeStrs(arr) {
-		var placeStr = '';
-		for (var i in arr) {
-			placeStr += '<li>' + arr[i] + '</li>';
-		}
-		return placeStr = '<ul>' + placeStr + '</ul>';
-	}
-	// 返回车牌结构
-	function cyclicLi(key) {
-		return '<div class="keyboard">\
-					  <div class="shut-down"><p>确定</p></div>\
-						<div class="place-letter">' + placeStrs(key) + '</div>\
-						</div>'
-	}
-	// 显示键盘
-	this.show = function () {
-		_this.okeyboard.style.display = 'block';
-	}
-	// 隐藏键盘
-	this.hide = function () {
-		_this.okeyboard.style.display = 'none';
-	}
-	// 点击确定，隐藏键盘
-	this.shutDown.addEventListener("click", function(event) {
+	var _this = this;                                            						  // 保存this
+	this._plate = document.getElementById(id);                   						  // 获取对象
+	this.plateNumber = this._plate.getAttribute("plateNumbers");						  // 获取自定义属性车牌号码
+	this.plateIndex = this.plateNumber.length;															  // 车牌键盘框索引
+	this.plateFrame = this._plate.getElementsByClassName("plate-frame")[0];  	// 车牌键盘框
+	this.plateFrameLi = this.plateFrame.getElementsByTagName("li");						// 键盘框li
+	this.okeyboard = this._plate.getElementsByClassName('keyboard')[0];       // 渲染完成获取键盘
+	this.placeLetter = this._plate.getElementsByClassName('place-letter')[0]; // 获取键盘
+	this.placeLetterLi = this.placeLetter.getElementsByTagName('li');         // 获取键盘li
+	this.shutDown = this._plate.getElementsByClassName('shut-down')[0];       // 获取关闭
+	this.queryDetail = this._plate.getElementsByClassName('queryDetail')[0]   // 查询按钮
+	this.initInput();																												  // 初始化车牌输入框
+	this.initShow(this.plateIndex);																					  // 键盘初始化
+	this.inputBox();																													// 初始化车牌框
+	this.keyboard();																													// 循环数字键盘
+	this.shutDown.addEventListener("click", function() {											// 点击确定，隐藏键盘
 		_this.hide();
 	})
-	// init(plateIndex);
-	// 显示地名键盘
-	function showOplaceName() {
-		// _this.show();
-		// _this._plate.removeChild(_this._plate.lastChild)
-		_this._plate.insertAdjacentHTML('beforeend', cyclicLi(PVS));
-		// _this._plate.replaceChild(cyclicLi(PVS), _this.okeyboard);
-	}
-	// 显示数字键盘
-	function showPlaceLetter() {
+	this.plateFrame.addEventListener("click", function() {										// 点击车牌框，显示键盘
 		_this.show();
-		// _this._plate.removeChild(_this._plate.lastChild)
-		// _this._plate.insertAdjacentHTML('beforeend', cyclicLi(NUM));
-		_this._plate.replaceChild(cyclicLi(NUM), _this.okeyboard);
+	})
+}
+Plate.prototype.initInput = function() {  // 初始化车牌框
+	for(var i=0; i<this.plateNumber.length; i++) {
+		PNUM[i] = this.plateNumber[i];
 	}
-	// 遍历车牌键盘框
-	this.inputBox = function () {
-		_this.plateNumber = '';
-		for(var i=0; i<_this.plateFrameLi.length; i++) {
-			_this.plateFrameLi[i].index = i;
-			_this.plateFrameLi[plateIndex].className = "active";
-			_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";
-			// 点击车牌框
-			_this.plateFrameLi[i].onclick = function() {
-				_this.show();
-				plateIndex = this.index;
-				// init(plateIndex);
-				// 修改车牌框样式
-				for(var j=0; j<_this.plateFrameLi.length-1; j++) {
-					_this.plateFrameLi[j].className = "";
-				}
-				if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML == '') {
-					_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";
-				}
-				this.className = "active";
+	this.plateFrame.innerHTML = placeStrs(PNUM);  // 将html自定义的车牌初始值赋给车牌框
+}
+Plate.prototype.initShow = function(index) {    // 初始化显示键盘
+	if(index == 0) {
+		this.placeLetter.innerHTML = placeStrs(PVS);
+	} else {
+		this.placeLetter.innerHTML = placeStrs(NUM);
+	}
+}
+Plate.prototype.hide = function() {  // 隐藏键盘
+	this.okeyboard.style.display = 'none';
+}
+Plate.prototype.show = function() {  // 显示键盘
+	this.okeyboard.style.display = 'block';
+}
+Plate.prototype.inputBox = function() {  // 遍历车牌键盘，获取车牌
+	var _this = this;
+	this.plateNumber = '';  // 先将车牌清空，后面再累加
+	for(var i=0; i<this.plateFrameLi.length; i++) {
+		this.plateFrameLi[i].index = i;
+		this.plateFrameLi[this.plateIndex].className = "active";
+		this.plateFrameLi[this.plateFrameLi.length-1].className = "new";
+		this.plateFrameLi[i].onclick = function() {  // 点击车牌框
+			_this.plateIndex = this.index;  // 获取车牌框的索引
+			for(var j=0; j<_this.plateFrameLi.length-1; j++) {
+				_this.plateFrameLi[j].className = "";  // 先清除所有车牌框的class
 			}
-			_this.plateNumber += _this.plateFrameLi[i].innerHTML;
+			if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML == '') {
+				_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";  // 如果新能源车牌框为空，则不去掉class，反之去掉
+			}
+			this.className = "active";  // 给点击当前车牌框加class
+			_this.initColor(_this.plateIndex);  // 根据车牌框索引，改变键盘颜色
+		}
+		this.plateNumber += this.plateFrameLi[i].innerHTML;  // 遍历车牌框，获取车牌
+		for(var k=0; k<_this.plateFrameLi.length-1; k++) {   // 如果车牌框输入不连续，则车牌为空
+			if(this.plateFrameLi[k].innerHTML == '') {
+				this.plateNumber = '';
+			}
 		}
 	}
-	this.inputBox();
-	// 循环数字键盘
-	function keyboard() {
-		for (var i = 0; i < _this.placeLetterLi.length; i++) {
-			_this.placeLetterLi[i].index = i;
-			var plength = _this.placeLetterLi.length - 1;
-			_this.placeLetterLi[plength].classList.add("lastli");
-			// init(plateIndex);
-			// 点击数字键盘
-			_this.placeLetterLi[i].onclick = function () {
-				if (this.index < plength) {
-					// 把点击键盘上的值给车牌框
-					if(this.innerHTML !== "")  {
-						_this.plateFrameLi[plateIndex].innerHTML = this.innerHTML;
-						_this.plateFrameLi[plateIndex].className = "";
-						if(plateIndex == _this.plateFrameLi.length-2) {
-							plateIndex = _this.plateFrameLi.length-2;
-						}
-						else if(plateIndex == _this.plateFrameLi.length-1) {
-							plateIndex = _this.plateFrameLi.length-1;
-						}
-						else {
-							plateIndex++;
-						}
+	if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML !== '') {
+		_this.plateFrameLi[_this.plateFrameLi.length-1].className = "";
+	}
+}
+Plate.prototype.keyboard = function() {  // 循环键盘，添加事件
+	var _this = this;
+	for (var i = 0; i < this.placeLetterLi.length; i++) {
+		this.placeLetterLi[i].index = i;
+		var plength = this.placeLetterLi.length - 1;
+		this.placeLetterLi[plength].classList.add("lastli");  // 给最后一个删除加class
+		this.initColor(this.plateIndex);
+		this.placeLetterLi[i].onclick = function () {  // 键点击事件
+			if (this.index < plength) {  // 非删除键
+				if(this.innerHTML !== "") {  // 车牌键非空
+					_this.plateFrameLi[_this.plateIndex].innerHTML = this.innerHTML;  // 取值赋值
+					_this.plateFrameLi[_this.plateIndex].className = "";
+					if(_this.plateIndex == _this.plateFrameLi.length-2) {  // 点击到新能源前一位，索引不变
+						_this.plateIndex = _this.plateFrameLi.length-2;
 					}
-					_this.plateFrameLi[plateIndex].className = "active";
-					// init();
-				} else if (this.index == plength) {
-					_this.plateFrameLi[plateIndex].innerHTML = "";
-					_this.plateFrameLi[plateIndex].className = "";
-					if(plateIndex == 1) {
-						plateIndex = 0;
-					}
-					else if(plateIndex == 0){
-						plateIndex = 0;
+					else if(_this.plateIndex == _this.plateFrameLi.length-1) {  // 点击到新能源，索引不变
+						_this.plateIndex = _this.plateFrameLi.length-1;
 					}
 					else {
-						plateIndex--;
-					}
-					// init();
-					_this.plateFrameLi[plateIndex].className = "active";
-					if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML == '') {
-						_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";
+						_this.plateIndex++;  // 其他情况索引累加
 					}
 				}
+				_this.plateFrameLi[_this.plateIndex].className = "active";  // 当前添加class
+			} else if (this.index == plength) {  // 当点击删除
+				_this.plateFrameLi[_this.plateIndex].innerHTML = "";
+				_this.plateFrameLi[_this.plateIndex].className = "";
+				if(_this.plateIndex == 1 || _this.plateIndex == 0) {  // 删除到最后一个的时候，索引不变
+					_this.plateIndex = 0;
+				}
+				else {
+					_this.plateIndex--;  // 索引累减
+				}
+				_this.plateFrameLi[_this.plateIndex].className = "active";  // 当前添加class
+				if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML == '') {  // 车牌框最后一个为空的时候，添加新能源class
+					_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";
+				}
 			}
+			_this.initColor(_this.plateIndex);  // 根据索引初始化键盘颜色
+			_this.forbidClick();
 		}
 	}
-	keyboard();
-	// 部分车牌键盘禁止点击
-	function bannedClick(id, item) {
-		id[item].classList.add("place-color");
-	}
-	// 清除键盘背景颜色
-	function clear(num) {
-		_this.placeLetterLi[num].classList.remove("place-color");
-	}
-	// 初始化键盘颜色
-	function init(index) {
-		if(index == 0) {
-			showOplaceName()
-			for (var i = 31; i < 38; i++) {
-				bannedClick(_this.placeLetterLi, i);
-			}
+}
+Plate.prototype.forbidClick = function() {
+	for (var i = 0; i < this.plateFrameLi.length-1; i++) {
+		var pl = '';
+		if(pl.length < 7) {
+			pl += this.plateFrameLi[i].innerHTML;
+			console.log(pl);
 		}
-		else if (index == 1) {
-			showPlaceLetter()
-			bannedClick(_this.placeLetterLi, 18);
-			bannedClick(_this.placeLetterLi, 19);
-			bannedClick(_this.placeLetterLi, 29);
-			bannedClick(_this.placeLetterLi, 37);
-			for (var j = 0; j < 10; j++) {
-				bannedClick(_this.placeLetterLi, j);
-			}
-		}
-		else if (index > 1 && index < 6) {
-			showPlaceLetter()
-			bannedClick(_this.placeLetterLi, 18);
-			bannedClick(_this.placeLetterLi, 19);
-			bannedClick(_this.placeLetterLi, 29);
-			bannedClick(_this.placeLetterLi, 37);
-			for (var k = 0; k < 10; k++) {
-				clear(k);
-			}
-		}
-		else if (index == 6) {
-			showPlaceLetter()
-			for (var i = 0; i < _this.placeLetterLi.length; i++) {
-				clear(i);
-			}
-		}
-		else if (index == 7) {
-			showPlaceLetter()
-			for (var i = 0; i < _this.placeLetterLi.length; i++) {
-				clear(i);
-			}
-			bannedClick(_this.placeLetterLi, 18);
-			bannedClick(_this.placeLetterLi, 19);
-			bannedClick(_this.placeLetterLi, 29);
-			bannedClick(_this.placeLetterLi, 37);
+		else if(pl.length > 6) {
+			this.queryDetail.classList.remove("noquery");
 		}
 	}
+}
+Plate.prototype.showOplaceName = function() {  // 循环显示地名键盘
+	for (var i = 0; i < this.placeLetterLi.length; i++) {
+		this.placeLetterLi[i].innerHTML = PVS[i];
+	}
+}
+Plate.prototype.showPlaceLetter = function() {  // 循环显示数字键盘
+	for (var i = 0; i < this.placeLetterLi.length; i++) {
+		this.placeLetterLi[i].innerHTML = NUM[i];
+	}
+}
+Plate.prototype.initColor = function(index) {  // 初始化背景颜色
+	if(index == 0) {
+		this.showOplaceName()
+		for (var i = 0; i < this.placeLetterLi.length; i++) {
+			clear(this.placeLetterLi, i);
+		}
+		for (var i = 31; i < 38; i++) {
+			bannedClick(this.placeLetterLi, i);
+		}
+	}
+	else if (index == 1) {
+		this.showPlaceLetter();
+		for (var i = 0; i < this.placeLetterLi.length; i++) {
+			clear(this.placeLetterLi, i);
+		}
+		bannedClick(this.placeLetterLi, 18);
+		bannedClick(this.placeLetterLi, 19);
+		bannedClick(this.placeLetterLi, 29);
+		bannedClick(this.placeLetterLi, 37);
+		for (var j = 0; j < 10; j++) {
+			bannedClick(this.placeLetterLi, j);
+		}
+	}
+	else if (index > 1 && index < 6) {
+		this.showPlaceLetter();
+		bannedClick(this.placeLetterLi, 18);
+		bannedClick(this.placeLetterLi, 19);
+		bannedClick(this.placeLetterLi, 29);
+		bannedClick(this.placeLetterLi, 37);
+		for (var k = 0; k < 10; k++) {
+			clear(this.placeLetterLi, k);
+		}
+	}
+	else if (index == 6) {
+		this.showPlaceLetter();
+		for (var i = 0; i < this.placeLetterLi.length; i++) {
+			clear(this.placeLetterLi, i);
+		}
+	}
+	else if (index == 7) {
+		this.showPlaceLetter();
+		for (var i = 0; i < this.placeLetterLi.length; i++) {
+			clear(this.placeLetterLi, i);
+		}
+		bannedClick(this.placeLetterLi, 18);
+		bannedClick(this.placeLetterLi, 19);
+		bannedClick(this.placeLetterLi, 29);
+		bannedClick(this.placeLetterLi, 37);
+	}
+}
+function placeStrs(arr) {  // 返回ul结构
+	var placeStr = '';
+	for (var i in arr) {
+		placeStr += '<li>' + arr[i] + '</li>';
+	}
+	return placeStr = '<ul>' + placeStr + '</ul>';
+}
+function bannedClick(id, item) {  // 添加背景颜色,禁止点击
+	id[item].classList.add("place-color");
+}
+function clear(id, num) {  // 清除背景颜色，可以点击
+	id[num].classList.remove("place-color");
 }
