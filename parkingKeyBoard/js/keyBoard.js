e98f39dfe49c6f45e6484f080b680a6268094f54
AsaZws <zhouws.com@gmail.com>
2020-08-05 19:40:30 +0800

    点击事件

--- parkingKeyBoard/js/keyBoard.js
+++ parkingKeyBoard/js/keyBoard.js
@@ -1,17 +1,4 @@
-/**
- * 1.保存this
- * 2.获取对象
- * 3.获取自定义的车牌
- * 4.获取输入框的id
- * 5.获取键盘的id
- * 6.获取关闭的id
- * 7.初始化键盘
- * 8.关闭键盘事件
- * 9.打开键盘事件
- * 车牌数字键盘 name：Asa_Zhou data：2020/04/13修改完成
- */
-
- ;(function(win, doc){
+;(function(win, doc){
 
 	var PVS = ['京', '津', '沪', '渝', '蒙', '新', '藏', '宁', '桂', '黑', '吉', '辽', '晋', '冀', '青', '鲁', '豫', '苏', '皖', '浙', '闽', '赣', '湘', '鄂', '琼', '甘', '陕', '粤', '云', '贵', '川', '', '', '', '', '', '', '', '<span>x</span>'];
 	var NUM = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'P', '港', '澳', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '学', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '警', '<span>x</span>'];
@@ -19,27 +6,30 @@
 
 	function Plate(p) {
 		var _self = this;
-		_self._plate = doc.getElementById(p.id);
+		_self._plate = doc.querySelector(p.id);
 		// 获取自定义初始车牌数组
 		_self.plateNumber = p.initPlateNumber.split('');
 		// 获取车牌框初始索引
 		_self.plateIndex = _self.plateNumber.length;
-		// 将初始车牌数组赋值给车牌数组
-		for(var i=0; i<_self.plateNumber.length; i++) {
-			PNUM[i] = _self.plateNumber[i];
-		}
-		console.log(_self);
-		
-		
+		_self.isShow = p.isShow;
+		console.log(_self.__proto__.init);
+		_self.__proto__.init();
 	}
 
 	Plate.prototype = {
 		init: function () {
-			this.initHtml();
-			this.bindEvent();
+			var _self = this;
+			_self.initHtml();
+			_self.bindEvent();
+			_self.keyboardEvent();
+			_self.isShow ? this.show() : this.hide();
 		},
 		initHtml: function () {
-			console.log(this);
+			var _self = this;
+			// 将初始车牌数组赋值给车牌数组
+			for(var i=0; i<_self.plateIndex; i++) {
+				PNUM[i] = _self.plateNumber[i]; 
+			}
 			var html = '<p>请输入车牌</p><div class="plate">';
 				html += '<div class="plate-frame"></div>';
 				html += '<div class="plate-button">'
@@ -48,34 +38,30 @@
 				html += '<div class="keyboard"><div class="shut-down"><p>确定</p></div>';
 				html += '<div class="place-letter"></div>';
 				html += '</div>'
-			this._plate.innerHTML = html;
+			_self._plate.innerHTML = html;
 			// 获取车牌框
-			this.plateFrame = this._plate.getElementsByClassName("plate-frame")[0];
+			this.plateFrame = this._plate.querySelector(".plate-frame");
 			// 将初始车牌的值渲染到车牌框
 			this.plateFrame.innerHTML = placeStrs(PNUM);
 			// 获取车牌框li
 			this.plateFrameLi = this.plateFrame.getElementsByTagName("li");
 			 // 获取键盘
-			this.placeLetter = this._plate.getElementsByClassName('place-letter')[0];
+			this.placeLetter = this._plate.querySelector('.place-letter');
 			// 渲染完成获取键盘
-			this.okeyboard = this._plate.getElementsByClassName('keyboard')[0];
+			this.okeyboard = this._plate.querySelector('.keyboard');
 			// 获取键盘li
 			this.placeLetterLi = this.placeLetter.getElementsByTagName('li');
 			// 根据初始车牌的长度为0渲染地名键盘，否则渲染数字字母键盘
-			if(this.plateIndex == 0) {
-				this.placeLetter.innerHTML = placeStrs(PVS);
-			} else {
-				this.placeLetter.innerHTML = placeStrs(NUM);
-			}
+			this.plateIndex == 0 ? this.placeLetter.innerHTML = placeStrs(PVS) : this.placeLetter.innerHTML = placeStrs(NUM);
 			// 获取关闭
-			this.shutDown = this._plate.getElementsByClassName('shut-down')[0];
+			this.shutDown = this._plate.querySelector('.shut-down');
 			// 查询按钮
-			this.queryDetail = this._plate.getElementsByClassName('queryDetail')[0];
+			this.queryDetail = this._plate.querySelector('.queryDetail');
 			// 点击关闭隐藏键盘、车牌框显示按钮，将点击将this改变为Plate对象
-			this.shutDown.addEventListener("click", function() {											// 点击确定，隐藏键盘
+			this.shutDown.addEventListener("click", function() {
 				this.hide();
 			}.bind(this))
-			this.plateFrame.addEventListener("click", function() {										// 点击车牌框，显示键盘
+			this.plateFrame.addEventListener("click", function() {
 				this.show();
 			}.bind(this))
 		},
@@ -88,7 +74,94 @@
 			this.okeyboard.style.display = 'block';
 		},
 		bindEvent: function () {
-			console.log(this.okeyboard);
+			var _self = this;
+			var len = this.plateFrameLi.length;
+			// 初始化车牌框样式
+			_self.plateFrameLi[len-1].className = "new";
+			_self.plateFrameLi[this.plateIndex].className = "active";
+			for(var i=0; i<len; i++) {
+				_self.plateFrameLi[i].index = i;
+				_self.plateFrameLi[i].onclick = function() {
+					_self.plateIndex = this.index;
+					for(var j=0; j<len-1; j++) {
+						// 先清除所有车牌框的class
+						_self.plateFrameLi[j].className = "";
+						if(_self.plateFrameLi[len-1].innerHTML == '') {
+							// 如果新能源车牌框为空，则不去掉class，反之去掉
+							_self.plateFrameLi[len-1].className = "new";
+						}
+						// 给点击当前车牌框加class
+						this.className = "active";
+						_self.showOplaceName();
+						// 根据车牌框索引，改变键盘颜色
+						// _this.initColor(_this.plateIndex);
+					}
+				}
+			}
+
+		},
+		keyboardEvent: function () {
+			var _self = this;
+			var len = this.placeLetterLi.length;
+			// 最后一个添加class
+			_self.placeLetterLi[len-1].classList.add("lastli");
+			for (var i = 0; i < len; i++) {
+				_self.placeLetterLi[i].index = i;
+				_self.showPlaceLetter();
+				_self.placeLetterLi[i].onclick = function () {
+					// 判断非空非删除键
+					if (this.index < len-1 && this.innerText !== "") {
+						// 取值赋值
+						_self.plateFrameLi[_self.plateIndex].innerHTML = this.innerHTML;
+						_self.plateFrameLi[_self.plateIndex].className = "";
+						// 点击到新能源前一位，索引不变
+						if(_self.plateIndex == _self.plateFrameLi.length-2) {
+							_self.plateIndex = _self.plateFrameLi.length-2;
+							_self.hide();
+						}
+						// 点击到新能源，索引不变
+						else if(_self.plateIndex == _self.plateFrameLi.length-1) {
+							_self.plateIndex = _self.plateFrameLi.length-1;
+							_self.hide();
+						}
+						// 其他情况索引累加
+						else {
+							_self.plateIndex++;
+						}
+						// 当前添加class
+						_self.plateFrameLi[_self.plateIndex].className = "active";
+					}
+					// 点击删除
+					else if (this.index == len-1) {
+						_self.plateFrameLi[_self.plateIndex].innerHTML = "";
+						_self.plateFrameLi[_self.plateIndex].className = "";
+						// 删除到最后一个的时候，索引不变
+						if(_self.plateIndex == 1 || _self.plateIndex == 0) {
+							_self.plateIndex = 0;
+						}
+						else {
+							_self.plateIndex--;  // 索引累减
+						}
+						// 当前添加class
+						_self.plateFrameLi[_self.plateIndex].className = "active";
+						if(_self.plateFrameLi[_self.plateFrameLi.length-1].innerHTML == '') {
+							// 车牌框最后一个为空的时候，添加新能源class
+							_self.plateFrameLi[_self.plateFrameLi.length-1].className = "new";
+						}
+					}
+					_self.showPlaceLetter();
+				}
+			}
+		},
+		showOplaceName: function () {
+			for (var i = 0; i < this.placeLetterLi.length; i++) {
+				this.placeLetterLi[i].innerHTML = PVS[i];
+			}
+		},
+		showPlaceLetter: function () {
+			for (var i = 0; i < this.placeLetterLi.length; i++) {
+				this.placeLetterLi[i].innerHTML = NUM[i];
+			}
 		}
 	}
 
@@ -108,54 +181,8 @@
 // function Plate(id) {															  // 车牌键盘框索引
 // 	this.inputBox();																													// 初始化车牌框
 // 	this.keyboard();																													// 循环数字键盘
-// 	this.shutDown.addEventListener("click", function() {											// 点击确定，隐藏键盘
-// 		_this.hide();
-// 	})
-// 	this.plateFrame.addEventListener("click", function() {										// 点击车牌框，显示键盘
-// 		_this.show();
-// 	})
 // }
-// Plate.prototype.initInput = function() {  // 初始化车牌框
-// 	for(var i=0; i<this.plateNumber.length; i++) {
-// 		this.PNUM[i] = this.plateNumber[i];
-// 	}
-// 	this.plateFrame.innerHTML = placeStrs(this.PNUM);  // 将html自定义的车牌初始值赋给车牌框
-// }
-// Plate.prototype.initShow = function(index) {    // 初始化显示键盘
-// 	if(index == 0) {
-// 		this.placeLetter.innerHTML = placeStrs(this.PVS);
-// 	} else {
-// 		this.placeLetter.innerHTML = placeStrs(this.NUM);
-// 	}
-// }
-// Plate.prototype.hide = function() {  // 隐藏键盘
-// 	this.okeyboard.style.display = 'none';
-// }
-// Plate.prototype.show = function() {  // 显示键盘
-// 	this.okeyboard.style.display = 'block';
-// }
-// Plate.prototype.inputBox = function() {  // 遍历车牌键盘，获取车牌
-// 	var _this = this;
-// 	for(var i=0; i<this.plateFrameLi.length; i++) {
-// 		this.plateFrameLi[i].index = i;
-// 		this.plateFrameLi[this.plateIndex].className = "active";
-// 		this.plateFrameLi[this.plateFrameLi.length-1].className = "new";
-// 		this.plateFrameLi[i].onclick = function() {  // 点击车牌框
-// 			_this.plateIndex = this.index;  // 获取车牌框的索引
-// 			for(var j=0; j<_this.plateFrameLi.length-1; j++) {
-// 				_this.plateFrameLi[j].className = "";  // 先清除所有车牌框的class
-// 			}
-// 			if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML == '') {
-// 				_this.plateFrameLi[_this.plateFrameLi.length-1].className = "new";  // 如果新能源车牌框为空，则不去掉class，反之去掉
-// 			}
-// 			this.className = "active";  // 给点击当前车牌框加class
-// 			_this.initColor(_this.plateIndex);  // 根据车牌框索引，改变键盘颜色
-// 		}
-// 	}
-// 	if(_this.plateFrameLi[_this.plateFrameLi.length-1].innerHTML !== '') {
-// 		_this.plateFrameLi[_this.plateFrameLi.length-1].className = "";
-// 	}
-// }
+
 // Plate.prototype.keyboard = function() {  // 循环键盘，添加事件
 // 	var _this = this;
 // 	for (var i = 0; i < this.placeLetterLi.length; i++) {
